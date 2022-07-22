11af3c2b229f1185c5c3605b1b0eb6779a5f9024
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-22 18:53:19 +0200

    handling loading and data states

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 
 function App() {
   const [movies, setmovies] = useState([]);
+  const [isloading, setisloading] = useState(false);
   function fetchMoviesHandler() {
+    setisloading(true);
     fetch('https://swapi.dev/api/films/').then(response => {
       return response.json();}).then(data => {
         const transfommovies = data.results.map((moviedata) => {
@@ -19,6 +21,7 @@ function App() {
           
         
         setmovies(transfommovies);
+        setisloading(false);
       });
     }
   
@@ -29,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isloading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isloading && movies.length === 0 && <p>Not Found Movies.</p>}
+        {isloading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
